36401cdf5cc9da80562eb185658f9bca2382f7b4
Zukiswa Lobola <Zukiswalobola26@gmail.com>
2020-02-28 15:28:28 +0200

    Changed directory choice to JFileChooser browser
    Exit now works properly.
    Added option to query server - still to do
    Selects files using browser but saves them to the default folder

---
+++ Server/test.js
@@ -0,0 +1,19 @@
+
+var youLikeMeat = true; 
+
+function myFunction() {
+	if (youLikeMeat) {
+		document.write("Here is my meaty menu");
+	}
+		
+}
+
+for (age = 0; age < 5; age++) {
+	document.write("you are "+age + "\n ");
+
+} 
+
+document.write("you are now 5");
+
+
+
